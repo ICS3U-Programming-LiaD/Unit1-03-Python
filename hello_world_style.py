7f84eac96b1216f3e092dbc0ba643d2e0a82d8c5
Lia Duggan <lia.duggan@stu.ocsb.ca>
2022-09-15 18:24:17 +0000

    made hello world with style

---
+++ hello_world_style.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Created By: Your Name
+# Date: Month Day, Year
+# Write what this program does.
+
+def main():
+    print("Hello World! with proper new style!")
+    print("")
+
+
+if __name__ == "__main__":
+    main()
